57481068700a0cfa16b81e71af740cc528eb109c
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-10-06 00:11:19 -0500

    Navegacion agregada

--- App.js
+++ App.js
@@ -24,6 +24,65 @@ const TIME = 100;
 const EASING = Easing.elastic(1.5);
 
 
+function HomeScreen({ navigation }) {
+
+  const animation = useRef(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+      <ImageBackground  source={require('./assets/background.jpg')} style={[styles.container]}>
+        <Animated.Image entering={BounceIn.duration(1000)} source={require('./assets/logo.png')} style={[styles.logo]} />
+        <Animated.Image entering={FadeIn.duration(2000)} source={require('./assets/planetas.png')} style={[styles.planetas]} />
+        {/* <Animated.Image entering={BounceIn.duration(3000)} source={require('./assets/astronauta.png')} style={[styles.astronauta]} />  */}
+        <View style={styles.astronauta}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 500,
+          height: 500,
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require('./assets/animation_astronauta.json')}
+      />
+      
+    </View>
+        <Animated.View entering={FadeIn.duration(2000)} style={styles.containerBtn}>
+          <TouchableOpacity onPress={() => navigation.navigate('Details')} style={styles.button}>
+            <Text style={styles.buttonText}>Iniciar</Text>
+          </TouchableOpacity>
+        </Animated.View>
+        
+      </ImageBackground>
+      </View>
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+      <ImageBackground  source={require('./assets/background.jpg')} style={[styles.container]}>
+        <Animated.Image entering={FadeIn.duration(2000)} source={require('./assets/planetas.png')} style={[styles.planetas]} />
+        <Animated.View entering={FadeIn.duration(2000)} style={styles.containerBtn}>
+          <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
+            <Text style={styles.buttonText}>Iniciar</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </ImageBackground>
+      </View>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App({ navigation }) {
   const rotation = useSharedValue(0);
@@ -54,41 +113,27 @@ export default function App({ navigation }) {
     // Esta función se ejecutará cuando se presione el botón
     console.log('Botón presionado');
   };
-  const animation = useRef(null);
-  useEffect(() => {
-    // You can control the ref programmatically, rather than using autoPlay
-    // animation.current?.play();
-  }, []);
+ 
 
   return (
-    <View style={styles.container}>
-      <ImageBackground  source={require('./assets/background.jpg')} style={[styles.container]}>
-        <Animated.Image entering={BounceIn.duration(1000)} source={require('./assets/logo.png')} style={[styles.logo]} />
-        <Animated.Image entering={FadeIn.duration(2000)} source={require('./assets/planetas.png')} style={[styles.planetas]} />
-        {/* <Animated.Image entering={BounceIn.duration(3000)} source={require('./assets/astronauta.png')} style={[styles.astronauta]} />  */}
-        <View style={styles.astronauta}>
-      <LottieView
-        autoPlay
-        ref={animation}
-        style={{
-          width: 500,
-          height: 500,
-        }}
-        // Find more Lottie files at https://lottiefiles.com/featured
-        source={require('./assets/animation_astronauta.json')}
-      />
-      
-    </View>
-        <Animated.View entering={FadeIn.duration(2000)} style={styles.containerBtn}>
-          <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
-            <Text style={styles.buttonText}>Iniciar</Text>
-          </TouchableOpacity>
-        </Animated.View>
-        
-        </ImageBackground>
-      </View>
-
-    
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            headerTransparent: true,
+          }}
+        />
+        <Stack.Screen 
+          name="Details" 
+          component={DetailsScreen} 
+          options={{
+            headerTransparent: true,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
