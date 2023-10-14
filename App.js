bbc5a5348ab2c1a4cc3357bd2c8f2f7b5e3c5a0c
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-10-14 00:08:53 -0500

    Agregados los iconos del tabs

--- App.js
+++ App.js
@@ -1,133 +1,224 @@
-import React, { useRef, useEffect } from 'react';
-import { StyleSheet, View, ImageBackground, TouchableOpacity, Text,  handleButtonPress,Button, navigation  } from 'react-native';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withRepeat,
-  FadeIn,
-  FadeInDown,
-  FadeOutDown,
-  FadeOut,
-  BounceIn,
-  BounceOut,
-  Easing,
-  withTiming,
-  withSequence,
-} from 'react-native-reanimated';
-import { useNavigation } from '@react-navigation/native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { BlurView } from 'expo-blur';
-
-import Planets from './src/Components/planets';
-import Tabs from './src/Components/tabs';
-
-import HomeScreen from './src/screens/HomeScreen';
-
-const ANGLE = 10;
-const TIME = 100;
-const EASING = Easing.elastic(1.5);
-
-const HomeStack = createNativeStackNavigator();
-
-function HomeStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-    </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createNativeStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
-  );
-}
+ import React, { useRef, useEffect } from 'react';
+ import { StyleSheet, View, Image, ImageBackground, TouchableOpacity, Text,  handleButtonPress,Button, navigation  } from 'react-native';
+ import Animated, {
+   useSharedValue,
+   useAnimatedStyle,
+   withRepeat,
+   FadeIn,
+   FadeInDown,
+   FadeOutDown,
+   FadeOut,
+   BounceIn,
+   BounceOut,
+   Easing,
+   withTiming,
+   withSequence,
+ } from 'react-native-reanimated';
+ import { useNavigation } from '@react-navigation/native';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ import Planets from './src/Components/planets';
+ import Tabs from './src/Components/tabs';
+ import HomeScreen from './src/screens/HomeScreen';
+ import SettingsScreen from './src/screens/SettingsScreen';
+ import SparklesScreen from './src/screens/SparklesScreen';
+ import BooksScreen from './src/screens/BooksScreen';
 
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={styles.container}>
-      <ImageBackground  source={require('./src/assets/images/background.jpg')} style={[styles.container]}>
-          <Planets></Planets>
-          <Animated.View entering={FadeIn.duration(2000)} style={styles.containerBtn}>
-            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
-              <Text style={styles.buttonText}>Iniciar</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        <Tabs></Tabs>
-      </ImageBackground>
-      </View>
-    </View>
-  );
-}
+ const ANGLE = 10;
+ const TIME = 100;
+ const EASING = Easing.elastic(1.5);
+ const HomeStack = createNativeStackNavigator();
+ function HomeStackScreen() {
+   return (
+     <HomeStack.Navigator>
+       <HomeStack.Screen 
+         name="Home" 
+         component={HomeScreen} 
+         options={{
+           headerShown: false,
+           tabBarVisibility: false
+         }}
+       />
+     </HomeStack.Navigator>
+   );
+ }
+ const SparklesStack = createNativeStackNavigator();
+ function SparklesStackScreen() {
+   return (
+     <SparklesStack.Navigator>
+       <SparklesStack.Screen 
+         name="SparklesStack" 
+         component={SparklesScreen} 
+         options={{
+           headerShown: false,
+         }}
+       />
+     </SparklesStack.Navigator>
+   );
+ }
+ const BooksStack = createNativeStackNavigator();
+ function BooksStackScreen() {
+   return (
+     <BooksStack.Navigator>
+       <BooksStack.Screen 
+         name="Books" 
+         component={BooksScreen} 
+         options={{
+           headerShown: false,
+         }}
+       />
+     </BooksStack.Navigator>
+   );
+ }
+ const SettingsStack = createNativeStackNavigator();
+ function SettingsStackScreen() {
+   return (
+     <SettingsStack.Navigator>
+       <SettingsStack.Screen 
+         name="Settings" 
+         component={SettingsScreen} 
+         options={{
+           headerShown: false,
+         }}
+       />
+     </SettingsStack.Navigator>
+   );
+ }
+//  function DetailsScreen({ navigation }) {
+//    return (
+//      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//        <View style={styles.container}>
+//        <ImageBackground  source={require('./src/assets/images/background.jpg')} style={[styles.container]}>
+//            <Planets></Planets>
+//            <Animated.View entering={FadeIn.duration(2000)} style={styles.containerBtn}>
+//              <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
+//                <Text style={styles.buttonText}>Iniciar</Text>
+//              </TouchableOpacity>
+//            </Animated.View>
+//          <Tabs></Tabs>
+//        </ImageBackground>
+//        </View>
+//      </View>
+//    );
+//  }
+ const Stack = createNativeStackNavigator();
+ const Tab = createBottomTabNavigator();
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+ export default function App({ navigation }) {
+   const rotation = useSharedValue(0);
+   const animatedStyle = useAnimatedStyle(() => ({
+     transform: [{ rotateZ: `${rotation.value}deg` }],
+   }));
+   React.useEffect(() => {
+     rotation.value = withSequence(
+       // deviate left to start from -ANGLE
+       withTiming(-ANGLE, { duration: TIME / 2, easing: EASING }),
+       // wobble between -ANGLE and ANGLE 7 times
+       withRepeat(
+         withTiming(ANGLE, {
+           duration: TIME,
+           easing: EASING,
+         }),
+         7,
+         true
+       ),
+       // go back to 0 at the end
+       withTiming(0, { duration: TIME / 2, easing: EASING })
+     );
+   }, []);
+   const handleButtonPress = () => {
+     // Esta función se ejecutará cuando se presione el botón
+     console.log('Botón presionado');
+   };
 
-export default function App({ navigation }) {
-  const rotation = useSharedValue(0);
+   return (
+    
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotateZ: `${rotation.value}deg` }],
-  }));
-
-  React.useEffect(() => {
-    rotation.value = withSequence(
-      // deviate left to start from -ANGLE
-      withTiming(-ANGLE, { duration: TIME / 2, easing: EASING }),
-      // wobble between -ANGLE and ANGLE 7 times
-      withRepeat(
-        withTiming(ANGLE, {
-          duration: TIME,
-          easing: EASING,
-        }),
-        7,
-        true
-      ),
-      // go back to 0 at the end
-      withTiming(0, { duration: TIME / 2, easing: EASING })
-    );
-  }, []);
-
-  const handleButtonPress = () => {
-    // Esta función se ejecutará cuando se presione el botón
-    console.log('Botón presionado');
-  };
- 
-
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        
-        screenOptions={{ 
-          headerShown: false,
-          tabBarStyle: {
-              height: 60,
-              position: 'absolute',
-              bottom: 16,
-              right: 16,
-              left: 16,
-              borderRadius: 25,
-              backgroundColor: 'rgba(255, 255, 255, 0.5)'
-          },
-          
-        }}>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            // tabBarIcon: ({ focused, color, size }) => {
+            //   return <CustomTab route={route} />;
+            // },
+            headerShown: false,
+            tabBarStyle: {
+                height: 60,
+                position: 'absolute',
+                bottom: 16,
+                right: 16,
+                left: 16,
+                borderRadius: 25,
+                backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            },
+            tabBarActiveTintColor: 'white'
+          })}
+         >
+          <Tab.Screen 
+            name="Home" 
+            component={HomeStackScreen} 
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({focused,color,size}) => {
+                if(focused){
+                  return (<Image source={require('./src/assets/icons/home-solid.png')} style={{ width: 24, height: 24 }} />);
+                }else{
+                  return (<Image source={require('./src/assets/icons/home-line.png')} style={{ width: 24, height: 24 }} />);
+                }
+              },
+              tabBarVisibility: false
+            }}
+          />
+          <Tab.Screen 
+            name="Sparkles" 
+            component={SparklesStackScreen} 
+            options={{
+              tabBarLabel: "Sparkles",
+              tabBarIcon: ({focused,color,size}) => {
+                if(focused){
+                  return (<Image source={require('./src/assets/icons/sparkles-solid.png')} style={{ width: 24, height: 24 }} />);
+                }else{
+                  return (<Image source={require('./src/assets/icons/sparkles-line.png')} style={{ width: 24, height: 24 }} />);
+                }
+              },
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen 
+            name="Books" 
+            component={BooksStackScreen} 
+            options={{
+              tabBarLabel: "Books",
+              tabBarIcon: ({focused,color,size}) => {
+                if(focused){
+                  return (<Image source={require('./src/assets/icons/books-solid.png')} style={{ width: 24, height: 24 }} />);
+                }else{
+                  return (<Image source={require('./src/assets/icons/books-line.png')} style={{ width: 24, height: 24 }} />);
+                }
+              },
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen 
+            name="Settings" 
+            component={SettingsStackScreen} 
+            options={{
+              tabBarLabel: "Settings",
+              tabBarIcon: ({focused,color,size}) => {
+                if(focused){
+                  return (<Image source={require('./src/assets/icons/setting-solid.png')} style={{ width: 24, height: 24 }} />);
+                }else{
+                  return (<Image source={require('./src/assets/icons/setting-line.png')} style={{ width: 24, height: 24 }} />);
+                }
+              },
+              headerShown: false,
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+   );
+ }
 
 const styles = StyleSheet.create({
   container: {
@@ -180,3 +271,4 @@ const styles = StyleSheet.create({
 
   },
 });
+
--- src/Components/tabs.jsx
+++ src/Components/tabs.jsx
@@ -1,68 +1,68 @@
-import React from 'react';
-import { View, StyleSheet, Text,Dimensions,Image, TouchableOpacity  } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+// import React from 'react';
+// import { View, StyleSheet, Text,Dimensions,Image, TouchableOpacity  } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
 
-const Tabs = () => {
-    const windowHeight = Dimensions.get('window').height;
-  const bottomViewHeight = 80; 
+// const Tabs = () => {
+//     const windowHeight = Dimensions.get('window').height;
+//   const bottomViewHeight = 80; 
 
-  const navigation = useNavigation();
+//   const navigation = useNavigation();
 
-  // Función para navegar a la pantalla correspondiente al tocar una imagen
-  const handleImagePress = (screenName) => {
-    navigation.navigate(screenName);
-  };
+//   // Función para navegar a la pantalla correspondiente al tocar una imagen
+//   const handleImagePress = (screenName) => {
+//     navigation.navigate(screenName);
+//   };
 
-  return (
-      <View style={[styles.bottomView, { top: windowHeight - bottomViewHeight }]}>
-        {/* Contenido del elemento View en la parte inferior */}
-        <View style={styles.tabs}>
-            <View style={styles.tabs2}>
-                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                    <Image source={require('../assets/icons/home.png')} style={styles.image} />
-                </TouchableOpacity>
-                <Image source={require('../assets/icons/section2.png')} style={styles.image} />
-                <Image source={require('../assets/icons/section3.png')} style={styles.image} />
-                <Image source={require('../assets/icons/section4.png')} style={styles.image} />
-            </View>
-        </View>
-      </View>
-  );
-};
+//   return (
+//       <View style={[styles.bottomView, { top: windowHeight - bottomViewHeight }]}>
+//         {/* Contenido del elemento View en la parte inferior */}
+//         <View style={styles.tabs}>
+//             <View style={styles.tabs2}>
+//                 <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+//                     <Image source={require('../assets/icons/home-solid.png')} style={styles.image} />
+//                 </TouchableOpacity>
+//                 <Image source={require('../assets/icons/home-solid.png')} style={styles.image} />
+//                 <Image source={require('../assets/icons/home-solid.png')} style={styles.image} />
+//                 <Image source={require('../assets/icons/home-solid.png')} style={styles.image} />
+//             </View>
+//         </View>
+//       </View>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  bottomView: {
-    position: 'absolute',
-    left: 30
-    // Otros estilos para el elemento en la parte inferior
-  },
-  tabs: {
-    width: Dimensions.get('window').width - 60,
-    height: 80,
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    borderRadius: 35,
-  },
-  tabs2: {
-    alignContent: '',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: Dimensions.get('window').width - 70,
-    height: 70,
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    borderRadius: 30,
-    margin: 5,
-    flexDirection: 'row',
-  },
-  image: {
-    width: 40,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 15, // Añade un margen para separar los elementos
-  }
-});
+// const styles = StyleSheet.create({
+//   bottomView: {
+//     position: 'absolute',
+//     left: 30
+//     // Otros estilos para el elemento en la parte inferior
+//   },
+//   tabs: {
+//     width: Dimensions.get('window').width - 60,
+//     height: 80,
+//     backgroundColor: 'rgba(255, 255, 255, 0.3)',
+//     borderRadius: 35,
+//   },
+//   tabs2: {
+//     alignContent: '',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     width: Dimensions.get('window').width - 70,
+//     height: 70,
+//     backgroundColor: 'rgba(255, 255, 255, 0.3)',
+//     borderRadius: 30,
+//     margin: 5,
+//     flexDirection: 'row',
+//   },
+//   image: {
+//     width: 40,
+//     height: 40,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     margin: 15, // Añade un margen para separar los elementos
+//   }
+// });
 
-export default Tabs;
+// export default Tabs;
   
   
   
